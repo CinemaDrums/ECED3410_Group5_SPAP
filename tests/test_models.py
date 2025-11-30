2ea295e7210ee2a05ab1523c976ed61ed5aa3812
Ashton <156637106+CinemaDrums@users.noreply.github.com>
2025-11-29 23:25:23 -0400

    Implement tests for Task and Student models

---
+++ tests/test_models.py
@@ -0,0 +1,23 @@
+import pytest
+from src.models import Task, Student
+
+# Requirement #9 from prof's email: Automated Testing
+# Run this by typing 'pytest' in terminal
+
+def test_create_task():
+    """checks if tasks init correctly"""
+    task = Task(1, "Lab 4", "2025-11-30", 20.0)
+    assert task.title == "Lab 4"
+    assert task.weight_percent == 20.0
+    assert task.status == "To Do" # default should be To Do
+
+def test_student_password_hashing():
+    """checks if bcrypt is actually hashing the password"""
+    student = Student("test@dal.ca", password_plain="secure123")
+    
+    # password should NOT be stored as plain text
+    assert student.password_hash != "secure123"
+    
+    # check if the verify function works
+    assert student.check_password("secure123") is True
+    assert student.check_password("wrongpass") is False
